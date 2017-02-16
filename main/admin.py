aa0c43c6eb38587e293a0bcf13847c3bdf063162
vebjornpf <vebjorn.fjeldberg@outlook.com>
2017-02-16 20:30:14 +0100

    ordnet på Admin-siden

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from.models import Subject, Theme, Exercise_Page
+
+
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    # doing this to make the Subject-table more transparent for the admins
+    search_fields = ['subject_code']
+    list_display = ('subject_code', 'subject_name', 'get_full_name')
+
+
+class ThemeAdmin(admin.ModelAdmin):
+    list_display = ['get_number_and_name','subject']
+    ordering = ['subject', 'chapter_number']
+
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Theme, ThemeAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+# These models are the core database that control the relationships between subject, themes
+# exercises, exercise pages and hints
+
+
+class Subject(models.Model):
+    subject_code = models.CharField(max_length=20)
+    subject_name = models.CharField(max_length=30)
+    professor_firstname = models.CharField(max_length=30)
+    professor_lastname = models.CharField(max_length=30)
+    professor_email = models.EmailField()
+
+    # nice when printing a Subject-object
+    def __str__(self):
+        return self.subject_code
+
+    def get_full_name(self):
+        return self.professor_firstname + " " + self.professor_lastname
+
+    get_full_name.short_description = 'Professor' # headline of professor-column in the admin-table
+
+
+class Theme(models.Model):
+    chapter_number = models.IntegerField(default=0)
+    chapter_name = models.CharField(max_length=30)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE) # to controll which subject the theme belongs to
+
+    # nice when printing a Theme-object
+    def __str__(self):
+        return "Kapittel " + str(self.chapter_number) + ": " + str(self.chapter_name)
+
+    def get_number_and_name(self):
+        return "Kapittel " + str(self.chapter_number) + ": " + str(self.chapter_name)
+    get_number_and_name.short_description = 'Chapter'
+
+class Exercise_Page(models.Model):
+    youtube_url = models.URLField()
+    headline = models.CharField(max_length=30)
+    explanation = models.CharField(max_length=30) # a short explanation for the exercise
+    theme = models.ForeignKey(Theme, on_delete=models.CASCADE) # to controll which theme the exercise_page belongs to
+
+
+#lass Exercise(models.Model):
+
+
+
+
