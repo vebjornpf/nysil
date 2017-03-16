af27310e7a86aeb6b96b063532abafce4bfcfaf6
vebjornpf <vebjorn.fjeldberg@outlook.com>
2017-03-16 19:25:24 +0100

    limt inn login-systemet

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,7 @@ app_name='main'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login_user/$', views.login_user, name='login_user'),
+    url(r'^logout_user/$', views.logout_user, name='logout_user'),
 ]
